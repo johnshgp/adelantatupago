3ac0a9a2f1c19c7a3d6c8f251b30ebbb359699bc
Ivan Arriola <74118394+Autodidacta-TI@users.noreply.github.com>
2021-12-23 17:32:27 -0300

    FIX validaciones con csv

--- l10n_co_e-invoice_bulk_load/models/import_invoice.py
+++ l10n_co_e-invoice_bulk_load/models/import_invoice.py
@@ -2,10 +2,10 @@
 
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+from datetime import datetime
 import base64
 import csv
 from datetime import date as dt
-
 
 class ImportInvoices(models.Model):
     _name = 'import.invoices'
@@ -46,17 +46,60 @@ class ImportInvoices(models.Model):
                 # Carga vals
                 if documento != '':
                     
-                    cliente_tmp = self.env['res.partner'].search([('vat','=',documento)])
-                    client_tercero_tmp = self.env['res.partner'].search([('vat','=',nit_nomina)])
-                    producto_tmp = self.env['product.template'].search([('default_code','=',producto)])
+                    if documento != '':
+                        cliente_tmp = self.env['res.partner'].search([('vat','=',documento)])
+                        if len(cliente_tmp) == 0:
+                            raise ValidationError("El CSV no se procesara porque el Cliente con Documento de empleado N° {0} no existe, en la linea {1}, contenido de linea: {2}".format(documento, i, line))
+
+                    else:
+                        raise ValidationError("El CSV no se procesara por falta de Documento Empleado en la linea {0}, contenido de linea: {1}".format(i, line))
+
+
+
+                    if nit_nomina != '': 
+                        characters = "."
+                        for x in range(len(characters)):
+                            nit_nomina = nit_nomina.replace(characters[x],"")
+                        if len(nit_nomina.split('-',2))>1:
+                             nit_nomina = nit_nomina.split('-',2)[0]
+
+                        client_tercero_tmp = self.env['res.partner'].search([('vat','=',nit_nomina)])
+
+                        if len(client_tercero_tmp) == 0:
+                            raise ValidationError("El CSV no se procesara porque el Tercero Sociado con vat N° {0} no existe, en la linea {1}, contenido de linea: {2}".format(nit_nomina, i, line))
+
+                    else:
+                        raise ValidationError("El CSV no se procesara por falta de Tercero Sociado en la linea {0}, contenido de linea: {1}".format(i, line))
+
+                    if producto != '':
+                        producto_tmp = self.env['product.template'].search([('default_code','=',producto)])
+                        if len(producto_tmp) == 0:
+                            raise ValidationError("El CSV no se procesara por no existir el Producto {0} en la linea {1}, contenido de linea: {2}".format(producto,i, line))
+                    else:
+                        raise ValidationError("El CSV no se procesara por falta de Producto en la linea {0}, contenido de linea: {1}".format(i, line))
+
+
+                    if fecha != '':
+                        fecha_dt = datetime.strptime(fecha, '%d/%m/%Y')
+                    else:
+                        raise ValidationError("El CSV no se procesara por falta de Fecha en la linea {0}, contenido de linea: {1}".format(i, line))
+
                     
+                    
+                    if precio == '':
+                        raise ValidationError("El CSV no se procesara por falta de Precio en la linea {0}, contenido de linea: {1}".format(i, line))
+                    else:
+                        precio = precio.replace('.','')
+                        precio = precio.replace(',','.')
+                        precio = float(precio.replace('\r',''))
+
                     move_line_vals = []
 
                     line =(0, 0, {'account_id': 176,
                                     'product_id': producto_tmp.id, 
                                     'quantity': 1, 
                                     'product_uom_id': producto_tmp.uom_id.id,
-                                    'price_unit': float(precio),
+                                    'price_unit': precio,
                                     'tax_ids': [(4, producto_tmp.taxes_id.id)],})
                     
                     move_line_vals.append(line)
@@ -71,6 +114,7 @@ class ImportInvoices(models.Model):
 
                     
                     # Carga vals
+                    vals['invoice_date'] = fecha_dt
                     if client_tercero_tmp: vals['tercero_relacionado'] = client_tercero_tmp.id
                     if cliente_tmp: vals['partner_id'] = cliente_tmp.id
 
