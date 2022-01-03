a58e17b56e074f14fba98508a81e9dd8a6a7ecff
Ivan Arriola <74118394+Autodidacta-TI@users.noreply.github.com>
2022-01-03 15:38:59 -0300

    Fix state DIAN

--- l10n_co_e-invoice/models/validate_invoice_cron.py
+++ l10n_co_e-invoice/models/validate_invoice_cron.py
@@ -11,21 +11,21 @@ class ValidateInvoiceCron(models.TransientModel):
         for i in inv_to_validate:
             i.action_post()
             i.validate_dian()
-            if i.state_dian_document == 'Exitoso':
+            if i.state_dian_document == 'exitoso':
                 i.pago_tercero()
             elif i.state_dian_document == 'Por validar':
                 i.validate_dian()
-                if i.state_dian_document == 'Exitoso':
+                if i.state_dian_document == 'exitoso':
                     i.pago_tercero()
 
-        inv_to_validate_dian = self.env['account.move'].search([('validate_cron','=',True),('state','=','posted'),('state_dian_document','!=','Exitoso')])
+        inv_to_validate_dian = self.env['account.move'].search([('validate_cron','=',True),('state','=','posted'),('state_dian_document','!=','exitoso')])
         for idian in inv_to_validate_dian:
             idian.validate_dian()
-            if idian.pago_tercero_creado == False and idian.state_dian_document == 'Exitoso':
+            if idian.pago_tercero_creado == False and idian.state_dian_document == 'exitoso':
                 idian.pago_tercero()
             elif idian.state_dian_document == 'Por validar':
                 idian.validate_dian()
-                if idian.pago_tercero_creado == False and idian.state_dian_document == 'Exitoso':
+                if idian.pago_tercero_creado == False and idian.state_dian_document == 'exitoso':
                     idian.pago_tercero()
         raise ValidationError('inv_to_validate: {0}, inv_to_validate_dian: {1}'.format(inv_to_validate,inv_to_validate_dian))                  
   
