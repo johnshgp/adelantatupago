38d334ee53da73be41bc711fb3498d6bafacd508
Ivan Arriola <74118394+Autodidacta-TI@users.noreply.github.com>
2022-01-03 16:36:16 -0300

    FIX test

--- l10n_co_e-invoice/models/validate_invoice_cron.py
+++ l10n_co_e-invoice/models/validate_invoice_cron.py
@@ -29,15 +29,15 @@ class ValidateInvoiceCron(models.TransientModel):
                     idian.pago_tercero()
                     
                     
-        inv_to_validate_dian_false = self.env['account.move'].sudo().search([('validate_cron','=',True),('state','=','posted'),('state_dian_document','=',False)])
-        for idianf in inv_to_validate_dian_false:
-            idianf.validate_dian()
-            if idianf.pago_tercero_creado == False and idianf.state_dian_document == 'exitoso':
-                idianf.pago_tercero()
-            elif idianf.state_dian_document == 'por_validar':
-                idianf.validate_dian()
-                if idianf.pago_tercero_creado == False and idianf.state_dian_document == 'exitoso':
-                    idianf.pago_tercero()
+        inv_to_validate_dian_false = self.env['account.move'].sudo().search([('validate_cron','=',True),('state','=','posted')])
+        #for idianf in inv_to_validate_dian_false:
+        #    idianf.validate_dian()
+        #    if idianf.pago_tercero_creado == False and idianf.state_dian_document == 'exitoso':
+        #        idianf.pago_tercero()
+        #    elif idianf.state_dian_document == 'por_validar':
+        #        idianf.validate_dian()
+        #        if idianf.pago_tercero_creado == False and idianf.state_dian_document == 'exitoso':
+        #            idianf.pago_tercero()
         invoice = self.env['account.move'].sudo().browse(16)
         raise ValidationError('inv_to_validate: {0}, inv_to_validate_dian: {1}, idianf: {2}********** Factura: {3}, state_dian{4}, state{5} '.format(inv_to_validate,inv_to_validate_dian,inv_to_validate_dian_false,invoice.name,invoice.state_dian_document,invoice.state))                  
   
