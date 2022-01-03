ff0220fe48cd37883eace1a1a0f423474bf15256
Ivan Arriola <74118394+Autodidacta-TI@users.noreply.github.com>
2022-01-03 19:28:09 -0300

    ADD invoice_cron_inherit

---
+++ adelantatupago/models/validate_invoice_cron_inherit.py
@@ -0,0 +1,22 @@
+from odoo import models, fields, api
+import logging
+_logger = logging.getLogger(__name__)
+
+class ValidateInvoiceCronInherit(models.TransientModel):
+    _inherit = "validate.invoice.cron"
+
+    def validate_invoice_payments(self):
+        result = super(ValidateInvoiceCronInherit, self).validate_invoice_payments()
+        inv_to_payments = self.env['account.move'].search([('validate_cron','=',True),('state','=','posted'),('pago_tercero_creado','=',False)])
+        for i in inv_to_payments:
+            if i.state_dian_document == 'exitoso':
+                i.pago_tercero()
+            elif i.state_dian_document == 'por_validar':
+                i.validate_dian()
+                if i.state_dian_document == 'exitoso':
+                    i.pago_tercero()
+        
+        
+        _logger.warning('inv_to_payments: {0}'.format(inv_to_payments))
+
+        return result     
